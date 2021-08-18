036bb88082c834d3236d074cc0dd3ebec2bf0e8d
Sophealin-Hul <80858775+Sophealin-Hul@users.noreply.github.com>
2021-08-18 18:41:56 -0400

    modified some new files

--- assignment/second-app/src/week-2/boxCard/Box.js
+++ assignment/second-app/src/week-2/boxCard/Box.js
@@ -5,6 +5,7 @@ function Box(props) {
     <div className = 'card' style = {{backgroundColor: props.bgcolor, width: "250px"}}>
         <h1>{props.title}</h1>
         <h2>{props.subtitle}</h2>
+        <hr></hr>
         <h3>{props.description}</h3>
     </div>
   );
